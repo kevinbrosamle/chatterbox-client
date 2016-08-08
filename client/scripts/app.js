b10e725c8f4e50986469dd2d0307c69c1390173b
kevinbrosamle <kevin.brosamle@gmail.com>
2016-08-08 16:30:19 -0700

    Implements Message Posting and Input Cleaning
    Cleans inputs by escaping angle brackets. Posts messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,29 @@
 // YOUR CODE HERE:
 
+
+setInterval(() => {
+  var displayMessage = messages => {
+    _.each(messages.results, data => {
+      console.log(data.objectId);
+      let username = data.username ? cleanInput(data.username) : '';
+      let text = data.text ? cleanInput(data.text) : '';
+      username && $('#chats').append(`${username}: ${text}`).append('<br>');  
+    });
+    
+
+  };
+  $.get('https://api.parse.com/1/classes/messages', displayMessage);
+
+}, 1000);
+
+
+var cleanInput = input => {
+  let output = input.split('');
+  let charArray = ['<', '>'];
+  for (let i = 0; i < output.length; i++) {
+    if (_.indexOf(charArray, output[i]) !== -1) {
+      output = output.splice(i++ - 1, 0, '\\');
+    }
+  }
+  return output.join(''); 
+};
